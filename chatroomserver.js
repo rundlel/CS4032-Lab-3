ca74bb6c72db642da56aea7bdb4aa0cf8631a872
laurarundle <rundlel@tcd.ie>
2016-11-16 18:54:58 +0000

    modifications in order to read in data from client

--- chatroomserver.js
+++ chatroomserver.js
@@ -3,28 +3,53 @@ var net = require('net');
 var chatroom1[];
 var chatroom2[];
 var chatroom3[];
-var message[];
 var clientNo =0;
 
 //note to self: create array to store user and the room they are in? for easy removal?
+//maybe use a map?
 
 net.createServer(function(socket){
-	clientNo++;
-	socket.name = "Client " + clientNo;
+	//clientNo++;
+	//socket.name = "Client " + clientNo;
 
 
-	chatClients.push(socket);
-	socket.write("Welcome " + socket.name + "/n");
-	broadcast(socket.name + " joined the chat", socket);
-
+	//chatClients.push(socket);
+	//socket.write("Welcome " + socket.name + "/n");
+	//broadcast(socket.name + " joined the chat", socket);
 
 
 	socket.on("data", function(message){
 
-		if(message.substring(0,14)==="JOIN_CHATROOM:")
+		if(message.toString().substring(0,14)==="JOIN_CHATROOM:")
 		{
 			//convert to array in order to extract room name and client name
-			var index=0;
+			var data = message.toString().split("/n");
+			var room = data[0].toString().split(" ");	
+
+			process.stdout.write(data[0] + "/n");
+			for( var i = 0; i<room.length; i++)
+			{
+				process.stdout.write(room[i] + " ");
+			}
+
+			if room[1] === "one"
+			{
+				process.stdout.write("successfully joined chatroom one");
+			}
+			else if room [1] === "two"
+			{
+
+			}
+			else if room[1] === "three"
+			{
+
+			}
+			else
+			{
+				socket.write("This chat room doesn't exist");
+			}
+			//convert to array in order to extract room name and client name
+			
 
 		}
 		else if(message.substring(0,15)==="LEAVE_CHATROOM:")
@@ -62,4 +87,4 @@ net.createServer(function(socket){
 		process.stdout.write(message);
 	}
 
-});.listen(7070);
+}).listen(7070);
